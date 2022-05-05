f85fc40356e32b5da3b8cfc0586f92be9ec2b134
dmtrth25 <vodolazhchenko25@gmail.com>
2022-05-05 14:44:53 +0300

    added dynamically close links

---
+++ scroll/app.js
@@ -0,0 +1,20 @@
+// set date
+const date = document.getElementById('date')
+date.innerHTML = new Date().getFullYear()
+
+// close links
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function() {
+   // linksContainer.classList.toggle('show-links')
+   const heightContainer = linksContainer.getBoundingClientRect().height
+   const linksHeight = links.getBoundingClientRect().height
+   console.log(linksHeight);
+   if(heightContainer === 0) {
+      linksContainer.style.height = `${linksHeight}px`
+   }  else {
+      linksContainer.style.height = 0
+   }
+})
