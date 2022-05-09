3b783054fc07bd9522c31740a406587b965ebf44
dmtrth25 <vodolazhchenko25@gmail.com>
2022-05-09 14:47:36 +0300

    got values and calculated them

--- countdown/app.js
+++ countdown/app.js
@@ -21,3 +21,45 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway')
+const deadline = document.querySelector('.deadline')
+const items = document.querySelectorAll('.deadline-format h4')
+
+let futureData = new Date(2022, 7, 2, 11, 40)
+
+const year = futureData.getFullYear()
+const hours = futureData.getHours()
+const minutes = futureData.getMinutes()
+
+let month = futureData.getMonth()
+month = months[month]
+
+const date = futureData.getDate()
+
+const day = weekdays[futureData.getDay()]
+
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} ${hours}:${minutes}am`
+
+// future time in ms
+const futureTime = futureData.getTime()
+
+function getRemainingTime() {
+  const today = new Date().getTime()
+  const t = futureTime - today
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  // calculate values
+  let days = t / oneDay
+  days = Math.floor(days)
+
+  let hours = Math.floor((t % oneDay) / oneHour)
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let seconds = Math.floor((t % oneMinute) / 1000)
+  // console.log(minutes)
+}
+getRemainingTime()
